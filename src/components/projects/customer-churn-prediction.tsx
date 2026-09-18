b5815eba68ca75dfa8f14e02faf91fa4249700f2
Ahmedbilloo <ajawed1995@gmail.com>
2026-09-18 16:01:38 +0500

    Fix churn project code snippet

--- src/components/projects/customer-churn-prediction.tsx
+++ src/components/projects/customer-churn-prediction.tsx
@@ -51,7 +51,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics (
+from sklearn.metrics import (
     accuracy_score, precision_score, recall_score,
     f1_score, roc_auc_score, classification_report,
     ConfusionMatrixDisplay
